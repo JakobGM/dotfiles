035452cc2c0e69b1bbe5908c205cdca5605d3189
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-04-21 11:14:54 +0000

    Sort aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -2,19 +2,23 @@
 
 # Aliases
 alias atlas='ssh jakobgm@atlas.orakel.ntnu.no'
+alias cask='brew cask'
 alias dnsflush='dscacheutil -flushcache;sudo killall -HUP mDNSResponder;say flushed'
-alias krypton='ssh -p 2222 kokekunster.no'
+alias kokperm='sudo chmod -R 755 /Users/jakobgm/kokekunster.no/img /Users/jakobgm/kokekunster.no/arkiv /Users/jakobgm/kokekunster.no/ressurser; sudo chgrp -R _www /Users/jakobgm/kokekunster.no/img /Users/jakobgm/kokekunster.no/arkiv /Users/jakobgm/kokekunster.no/ressurser'
 alias koku='ssh -t kokekunster.no "cd /var/www/kokekunster.no/public_html ; bash"'
 alias kokut='ssh -t kokekunster.no "cd /var/www/test.kokekunster.no/public_html/kokekunster.no ; bash"'
+alias krypton='ssh -p 2222 kokekunster.no'
+alias logstud='ssh jakobgm@login.stud.ntnu.no'
+alias markov='ssh -Y jakobgm@markov.math.ntnu.no'
+alias matlab='cd /Applications/MATLAB_R2016a.app/bin; ./matlab -nodesktop -nosplash'
+alias orakelportal='ssh -t kokekunster.no "cd /var/www/portal.orakel.ntnu.no/public_html ; bash"'
+alias please='sudo !!'
+alias silicon='ssh jakobgm@188.166.127.82'
+alias thor='thorium'
+alias thorium='ssh jakobgm@thorium.orakel.ntnu.no'
 alias xenon='ssh jakobgm@kokekunster.no'
 alias zeus='ssh jakobgm@zeus.orakel.ntnu.no'
-alias orakelportal='ssh -t kokekunster.no "cd /var/www/portal.orakel.ntnu.no/public_html ; bash"'
-alias silicon='ssh jakobgm@188.166.127.82'
-alias kokperm='sudo chmod -R 755 /Users/jakobgm/kokekunster.no/img /Users/jakobgm/kokekunster.no/arkiv /Users/jakobgm/kokekunster.no/ressurser; sudo chgrp -R _www /Users/jakobgm/kokekunster.no/img /Users/jakobgm/kokekunster.no/arkiv /Users/jakobgm/kokekunster.no/ressurser'
-alias matlab='cd /Applications/MATLAB_R2016a.app/bin; ./matlab -nodesktop -nosplash'
-alias thorium='ssh jakobgm@thorium.orakel.ntnu.no'
-alias thor='thorium'
-alias logstud='ssh jakobgm@login.stud.ntnu.no'
 alias zshconfig='vim $HOME/.zshrc'
-alias markov='ssh -Y jakobgm@markov.math.ntnu.no'
 
+# Functions
+eval $(thefuck --alias)
