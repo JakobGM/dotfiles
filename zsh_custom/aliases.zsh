b323e7ccb7d42490063ad002347857a3fe266d1d
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-04-26 13:51:43 +0200

    Clean up alias file and add git aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,24 +1,40 @@
 #!/bin/zsh
 
-# Aliases
-alias atlas='ssh jakobgm@atlas.orakel.ntnu.no'
+# MacOS aliases
 alias cask='brew cask'
 alias dnsflush='dscacheutil -flushcache;sudo killall -HUP mDNSResponder;say flushed'
+alias matlab='cd /Applications/MATLAB_R2016a.app/bin; ./matlab -nodesktop -nosplash'
+
+
+# Misc aliases
 alias kokperm='sudo chmod -R 755 /Users/jakobgm/kokekunster.no/img /Users/jakobgm/kokekunster.no/arkiv /Users/jakobgm/kokekunster.no/ressurser; sudo chgrp -R _www /Users/jakobgm/kokekunster.no/img /Users/jakobgm/kokekunster.no/arkiv /Users/jakobgm/kokekunster.no/ressurser'
+alias please='sudo !!'
+alias thor='thorium'
+alias zshconfig='vim $HOME/.zshrc'
+
+
+# Tmux aliases
+alias ta='tmux attach -t'
+alias tn='tmux new -s'
+alias tl='tmux ls'
+
+
+# SSH aliases
+alias atlas='ssh jakobgm@atlas.orakel.ntnu.no'
 alias koku='ssh -t kokekunster.no "cd /var/www/kokekunster.no/public_html ; bash"'
 alias kokut='ssh -t kokekunster.no "cd /var/www/test.kokekunster.no/public_html/kokekunster.no ; bash"'
 alias krypton='ssh -p 2222 kokekunster.no'
 alias logstud='ssh jakobgm@login.stud.ntnu.no'
 alias markov='ssh -Y jakobgm@markov.math.ntnu.no'
-alias matlab='cd /Applications/MATLAB_R2016a.app/bin; ./matlab -nodesktop -nosplash'
 alias orakelportal='ssh -t kokekunster.no "cd /var/www/portal.orakel.ntnu.no/public_html ; bash"'
-alias please='sudo !!'
 alias silicon='ssh jakobgm@188.166.127.82'
-alias thor='thorium'
 alias thorium='ssh jakobgm@thorium.orakel.ntnu.no'
 alias xenon='ssh jakobgm@kokekunster.no'
 alias zeus='ssh jakobgm@zeus.orakel.ntnu.no'
-alias zshconfig='vim $HOME/.zshrc'
-alias ta='tmux attach -t'
-alias tn='tmux new -s'
-alias tl='tmux ls'
+
+
+# Git aliases
+alias g='git'
+alias gA='git add -A'
+alias gc='git commit'
+alias gp='git push'
