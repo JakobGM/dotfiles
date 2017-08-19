8aa37841271f0699c77c6fd8a2ac612067c7c17f
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-08-20 00:05:55 +0200

    Open (n)vim with n/v or current folder with nn/vv

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -3,7 +3,6 @@
 # MacOS aliases
 alias cask='brew cask'
 alias dnsflush='dscacheutil -flushcache;sudo killall -HUP mDNSResponder;say flushed'
-alias matlab='cd /Applications/MATLAB_R2016a.app/bin; ./matlab -nodesktop -nosplash'
 
 
 # Misc aliases
@@ -20,17 +19,17 @@ alias tl='tmux ls'
 
 
 # SSH aliases
-alias atlas='ssh jakobgm@atlas.orakel.ntnu.no'
-alias koku='ssh -t kokekunster.no "cd /var/www/kokekunster.no/public_html ; bash"'
-alias kokut='ssh -t kokekunster.no "cd /var/www/test.kokekunster.no/public_html/kokekunster.no ; bash"'
-alias krypton='ssh -p 2222 kokekunster.no'
-alias logstud='ssh jakobgm@login.stud.ntnu.no'
-alias markov='ssh -Y jakobgm@markov.math.ntnu.no'
-alias orakelportal='ssh -t kokekunster.no "cd /var/www/portal.orakel.ntnu.no/public_html ; bash"'
-alias silicon='ssh jakobgm@188.166.127.82'
-alias thorium='ssh jakobgm@thorium.orakel.ntnu.no'
-alias xenon='ssh jakobgm@kokekunster.no'
-alias zeus='ssh jakobgm@zeus.orakel.ntnu.no'
+alias atlas='mosh jakobgm@atlas.orakel.ntnu.no'
+alias koku='mosh -t kokekunster.no "cd /var/www/kokekunster.no/public_html ; bash"'
+alias kokut='mosh -t kokekunster.no "cd /var/www/test.kokekunster.no/public_html/kokekunster.no ; bash"'
+alias krypton='mosh -p 2222 kokekunster.no'
+alias logstud='mosh jakobgm@login.stud.ntnu.no'
+alias markov='mosh -Y jakobgm@markov.math.ntnu.no'
+alias orakelportal='mosh -t kokekunster.no "cd /var/www/portal.orakel.ntnu.no/public_html ; bash"'
+alias silicon='mosh jakobgm@188.166.127.82'
+alias thorium='mosh jakobgm@thorium.orakel.ntnu.no'
+alias xenon='mosh jakobgm@kokekunster.no'
+alias zeus='mosh jakobgm@zeus.orakel.ntnu.no'
 
 
 # Git aliases
@@ -38,3 +37,13 @@ alias g='git'
 alias gA='git add -A'
 alias gc='git commit'
 alias gp='git push'
+alias s='git status'
+alias ss='git status --staged'
+
+
+# Application aliases
+alias n='nvim'
+alias nn='nvim ..'
+alias v='vim'
+alias vv='vim .'
+alias matlab='cd /Applications/MATLAB_R2016a.app/bin; ./matlab -nodesktop -nosplash'
