75c0250f61d7851c489eb315a52794039a0761fa
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-10-24 21:31:33 +0200

    Symlink $HOME/.config

--- scripts/symlinker.zsh
+++ scripts/symlinker.zsh
@@ -8,8 +8,28 @@ setopt glob_dots
 # Symlink all home directory dotfiles
 ln -svf $DOTREPO/home/* $HOME
 
-# Symlink all configuration folders to .config
-ln -svf $DOTREPO/config/* $HOME/.config
-
 # Symlink all ZSH_CUSTOM files
 ln -svf $DOTREPO/zsh_custom/* $HOME/.oh-my-zsh/custom
+
+# This only matches when there exists a symlink already, which also
+# points to the correct folder in the dotfile repo. NB! Not POSIX compliant
+# See: https://unix.stackexchange.com/questions/192294/
+if [ "$HOME/.config" -ef "$DOTREPO/config" ]
+then
+    echo ".config is already symlinked to dotfiles repo"
+else
+    echo "\$HOME/.config has not been symlinked to $DOTREPO/config"
+
+    # Confirm if we want to symlink
+    read -q "REPLY?Symlink now (y/n)?: "
+    echo
+
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        # Keep backup of old $HOME/.config
+        mv $HOME/.config $HOME/.config.old
+
+        # Symlink .config to dotrepo, as not all applications respect $XDG_CONFIG_HOME
+        ln -svf $DOTREPO/config $HOME/.config
+    fi
+fi
