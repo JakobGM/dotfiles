384f30b25176a06b0bf3138fe4831ce2ccf4ef3d
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-01-13 16:14:01 +0100

    Better handling of unavailable packages

--- archlinux/install_aur_packages.sh
+++ archlinux/install_aur_packages.sh
@@ -4,4 +4,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf "\nInstalling AUR packages\n"
-yaourt -S --needed $(cat $DIR/aur_pkglist.txt | egrep -v "^\s*(#|$)")
+
+# Filter out comments from the file containing which packages to install
+for package in $(cat $DIR/aur_pkglist.txt | egrep -v "^\s*(#|$)")
+do
+    # Check if the package can be found in the registry
+    if [[ $(yaourt -Ss -q $package | egrep ^${package}$) ]]
+    then
+        printf "\n------------------------- Installing $package -------------------------"
+        yaourt -S --needed $package
+    else
+        echo -e "\033[0;31mCould not find the package '$package'!"
+        exit 1
+    fi
+done
--- archlinux/install_pacman_packages.sh
+++ archlinux/install_pacman_packages.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env sh
 
+# Update package registry
+sudo packman -y
+
 # Get the path of the parent directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf "\nInstalling pacman packages\n"
-sudo pacman -Syu --needed $(cat $DIR/pacman_pkglist.txt | egrep -v "^\s*(#|$)")
+
+# Filter out comments from the file containing which packages to install
+for package in $(cat $DIR/pacman_pkglist.txt | egrep -v "^\s*(#|$)")
+do
+    # Check if the package can be found in the registry
+    if [[ $(pacman -Ss "^$package\$") ]]
+    then
+        printf "\n------------------------- Installing $package -------------------------"
+        sudo pacman -Su --needed $package
+    else
+        echo -e "\033[0;31mCould not find the package '$package'"
+        exit 1
+    fi
+done
