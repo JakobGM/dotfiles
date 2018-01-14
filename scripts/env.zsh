703a65e33e6d187e5536841614f3017fd3e55146
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-01-14 20:23:45 +0100

    Add HiDPI support for i3/Xorg/polybar

--- scripts/env.zsh
+++ scripts/env.zsh
@@ -105,3 +105,8 @@ if [[ $OS == "GNU/Linux" ]]
 then
     export BROWSER=xdg-open
 fi
+
+# --- HiDPI support in ArchLinux ---
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
