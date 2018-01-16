eeb4b15ed325d8b6286cb3fb49ed7d4d8a7b31f3
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-01-16 22:56:46 +0100

    Handle external displays on i3 reload

---
+++ config/mons/setup_displays.zsh
@@ -0,0 +1,50 @@
+#!/usr/bin/env zsh
+local EXTERNAL_MONITOR='HDMI2'
+local INTERNAL_MONITOR='eDP1'
+
+local hdmi_connected=$(mons | grep "$EXTERNAL_MONITOR")
+local hdmi_enabled=$(mons | grep "$EXTERNAL_MONITOR    (enabled)")
+local internal_enabled=$(mons | grep "$INTERNAL_MONITOR    (enabled)")
+
+
+echo '-------------------------------'
+echo 'Found the following information'
+echo $hdmi_enabled
+echo $hdmi_connected
+echo $internal_enabled
+echo '-------------------------------'
+
+if [ $hdmi_connected != '' ] 
+then
+    if [ $hdmi_enabled != '' ]
+    then
+        echo "Already connected to external display: $EXTERNAL_MONITOR"
+
+        if [ $internal_enabled -e '' ]
+        then
+            echo "Internal display $INTERNAL_MONITOR is not activated."
+            echo 'Activating it now...'
+            # Start the mons daemon in order to automatically start a display if it only detects one monitor
+            mons -o
+        else
+            echo "Internal display $INTERNAL_MONITOR is also already activated"
+        fi
+    else
+        echo "Connecting to external display: $EXTERNAL_MONITOR"
+
+        # Assuming that the laptop screen is to the *right* of the primary display
+        mons --primary $EXTERNAL_MONITOR -e right
+    fi
+else
+    echo "External display $EXTERNAL_MONITOR not connected"
+    echo "Only using internal monitor"
+fi
+
+if [ $hdmi_connected != '' ]
+then
+    if [ $duplicate_mode != '' ] 
+    then
+        echo "Duplicate mode is enabled, extending monitors instead"
+        mons --primary $EXTERNAL_MONITOR -e right
+    fi
+fi
