67e4278e7cb808e628c542594ea4c36ca2ebc4d8
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-01-17 16:46:46 +0100

    Add i3scripts for automatic workspace renaming
    And creating new workspaces

---
+++ archlinux/install_i3_dependencies.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+if [[ -v DOTREPO ]]
+then
+    cd $DOTREPO
+    git submodule update --recursive
+    mkvirtualenv -p python3.6 -a "$DOTREPO/archlinux/now_playing" i3
+    workon i3
+
+    # Dependency of now_playing
+    pip install requests
+
+    # Dependencies of i3scripts
+    pip install i3ipc fontawesome
+    sudo pacman -Syu xorg-xprop
+
+    deactivate
+fi
--- archlinux/install_now_playing.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-if [[ -v DOTREPO ]]
-then
-    cd $DOTREPO
-    git submodule update --recursive
-    mkvirtualenv -p python3.6 -a "$DOTREPO/archlinux/now_playing" now_playing
-    pip install requests
-fi
