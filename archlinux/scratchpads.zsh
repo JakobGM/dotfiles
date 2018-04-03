89fce38be046e706ac561871eb14d06e736ff452
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-04-03 22:00:21 +0200

    Add documentation viewer scratchpad to i3wm

--- archlinux/scratchpads.zsh
+++ archlinux/scratchpads.zsh
@@ -10,6 +10,11 @@ then
     alacritty --dimensions 0 0 --title dropdown_ipython -e ipython
 fi
 
+if ! pgrep -f zeal > /dev/null
+then
+    zeal
+fi
+
 # Try to start dropdown_ipython again if it panics on startup (for some reason)
 if ! pgrep -f dropdown_ipython > /dev/null
 then
