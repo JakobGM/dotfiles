1ca4fad8b31f09b7a526c40faf9313a7e00130e1
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-09-09 15:00:42 +0200

    zsh: Lazyload the pyenv binary

--- scripts/paths.zsh
+++ scripts/paths.zsh
@@ -2,15 +2,25 @@
 
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
-  # Add pyenv shims and autocompletion
-  eval "$(pyenv init -)"
 
-  # Add auto-activation of virtualenvs
-  eval "$(pyenv virtualenv-init -)"
+  # Lazyload the pyenv binary
+  function pyenv() {
+      # Remove the function definition
+      unfunction "$0"
+
+      # Add pyenv shims and autocompletion
+      eval "$(pyenv init -)"
+
+      # Add auto-activation of virtualenvs
+      eval "$(pyenv virtualenv-init -)"
+
+      # Execute the command that was intended
+      $0 "$@"
+  }
 
   # Setup virtualenvwrapper commands
   # PS: This also installs virtualenvwrapper if missing
   source ~/.local/bin/virtualenvwrapper_lazy.sh
   # pyenv virtualenvwrapper
-  export VIRTUALENV_PYTHON="$(pyenv which python3)"
+  export VIRTUALENV_PYTHON="/opt/pyenv/versions/3.6.6/bin/python3"
 fi
