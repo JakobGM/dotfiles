746f5f44fdcda3011fd79948b286bdb645c6cc6a
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-09-09 13:47:02 +0200

    zsh: Remove all history based plugins
    They do not work at the moment anyway...

--- autoload/functions.zsh
+++ autoload/functions.zsh
@@ -10,31 +10,6 @@ then
     source $NMAN
 fi
 
-# This will find the most frequently issued command issued exactly in this directory,
-# or if there are no matches it will find the most frequently issued command in any directory.
-_zsh_autosuggest_strategy_histdb_top() {
-    local query="select commands.argv from
-history left join commands on history.command_id = commands.rowid
-left join places on history.place_id = places.rowid
-where commands.argv LIKE '$(sql_escape $1)%'
-group by commands.argv
-order by places.dir != '$(sql_escape $PWD)', count(*) desc limit 1"
-    suggestion=$(_histdb_query "$query")
-}
-# ZSH_AUTOSUGGEST_STRATEGY=histdb_top
-
-# Precmd hook in order to get correct timing information for 'zsh-histdb'
-autoload -Uz add-zsh-hook
-add-zsh-hook precmd histdb-update-outcome
-
-function zshexit() {
-    # Sync the history database on every exit
-    histdb-sync
-
-    # Deactivate venv on exit
-    [ "$VIRTUAL_ENV" ] && deactivate
-}
-
 function fixsystemdboot() {
     # Mount the EFI partition
     mkdir -p /Volumes/EFI
