6a2bde561c149ac95b4c016d272409c9be4df5d5
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-05-08 21:21:08 +0200

    i3: Use ipython for python scratchpad terminal

--- archlinux/ipython.zsh
+++ archlinux/ipython.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
-source $(which virtualenvwrapper.sh)
-workon otovo
-alias shell='python manage.py shell_plus --ipython'
-shell
+ipython
+
+# Uncomment in order to invoke the shell in a specific django project
+# source $(which virtualenvwrapper.sh)
+# workon otovo
+# alias shell='python manage.py shell_plus --ipython'
+# shell
