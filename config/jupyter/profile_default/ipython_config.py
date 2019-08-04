b4ce140bee79b228c8d7c13e9d9bc6d0901cd51b
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-08-04 19:41:55 +0200

    ipython: Guard against powerline import failure

--- config/jupyter/profile_default/ipython_config.py
+++ config/jupyter/profile_default/ipython_config.py
@@ -1,8 +1,11 @@
-from powerline.bindings.ipython.since_5 import PowerlinePrompts
-
 c = get_config()
-c.TerminalInteractiveShell.simple_prompt = False
-c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
+
+try:
+    from powerline.bindings.ipython.since_5 import PowerlinePrompts
+    c.TerminalInteractiveShell.simple_prompt = False
+    c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
+except ModuleNotFoundError:
+    pass
 
 c.InteractiveShellApp.exec_files = [
     'autoimport.py',
