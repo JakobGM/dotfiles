ec49cfe3bfc976800204311b8b112513bb78879b
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-09-25 19:22:56 +0200

    i3wm: Use project-thesis ipython scratchpad

--- archlinux/ipython.zsh
+++ archlinux/ipython.zsh
@@ -1,9 +1,4 @@
 #!/usr/bin/env zsh
-workon sunmapper
-docker exec -it sunmapper_django python3 manage.py shell_plus --ipython
-
-# Uncomment in order to invoke the shell in a specific django project
-# source $(which virtualenvwrapper.sh)
-# workon otovo
-# alias shell='python manage.py shell_plus --ipython'
-# shell
+source ~/.virtualenvs/project/bin/activate
+cd ~/dev/project-thesis
+ipython
