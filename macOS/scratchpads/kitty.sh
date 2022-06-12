fe82100b8b5e86ab6ec18e4aa79b3065fc776674
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2022-06-13 00:15:34 +0200

    macos: Add yabai and skhdrc configs

---
+++ macOS/scratchpads/kitty.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+scratchpad_id=$(yabai -m query --windows | jq '.[] | select(.title=="Scratchpad").id')
+
+if [[ "$scratchpad_id" -lt 1 ]]; then
+  kitty --title='Scratchpad' | awk '{print $NF}' &
+  scratchpad_id=$(yabai -m query --windows | jq '.[] | select(.title=="Scratchpad").id')
+  sleep 1
+  yabai -m window --focus "$scratchpad_id"
+  yabai -m window --toggle float
+  yabai -m window --grid 16:16:3:3:10:10
+else
+  is_minimized=$(yabai -m query --windows --window "$scratchpad_id" | jq '.["is-minimized"]')
+  current_space=$(yabai -m query --spaces --space | jq '.index')
+
+  if [[ "$is_minimized" = "true" ]]; then
+    yabai -m window "$scratchpad_id" --space "$current_space"
+    yabai -m window --focus "$scratchpad_id"
+    yabai -m window --grid 16:16:3:3:10:10
+  else
+    yabai -m window "$scratchpad_id" --minimize
+  fi
+fi
