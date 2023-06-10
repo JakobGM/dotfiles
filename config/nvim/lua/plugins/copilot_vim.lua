c552f3b6ed1de8b035c1a6af5544377f97a0f0af
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-06-10 15:54:09 +0200

    vim: Use lua plugin for GitHub copilot

--- config/nvim/lua/plugins/copilot_vim.lua
+++ config/nvim/lua/plugins/copilot_vim.lua
@@ -1,4 +1,51 @@
 -- GitHub Copilot integration
 return {
-    "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<C-Space>",
+          refresh = "gr",
+          open = "<C-s>"
+        },
+        layout = {
+          position = "right", -- | top | left | right
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-Space>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-h>",
+          prev = "<C-g>",
+          dismiss = "<C-d>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 16.x
+      server_opts_overrides = {},
+    })
+  end,
 }
