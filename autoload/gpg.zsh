3747e11a22a78cb71970fe758a8d8f2b7e605e83
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-07-15 15:08:06 +0200

    sway: Set correct environment variables for gpg agent

---
+++ autoload/gpg.zsh
@@ -0,0 +1,13 @@
+# Fix issue with VSCode not getting access to the system keychain
+# https://code.visualstudio.com/docs/editor/settings-sync#_troubleshooting-keychain-issues
+export $(dbus-launch)
+
+# See https://wiki.archlinux.org/title/GNOME/Keyring#xinitrc
+export $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
+export SSH_AUTH_SOCK
+
+# See https://github.com/NixOS/nixpkgs/issues/14966#issuecomment-520083836
+mkdir -p "$HOME/.local/share/keyrings"
+
+# See https://unix.stackexchange.com/a/690294
+eval "$(echo '\n' | gnome-keyring-daemon --unlock)"
