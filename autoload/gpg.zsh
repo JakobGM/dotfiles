c692ffe56621316f55e78c5fe6f9f9b43c0f5417
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-08-24 17:17:40 +0200

    keyring: Kill duplicate gnome keyring daemons
    Let 1Password start them instead.
    
    Can be seen with ps ax | rg keyring

--- autoload/gpg.zsh
+++ autoload/gpg.zsh
@@ -6,8 +6,13 @@ export $(dbus-launch)
 # export $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
 # export SSH_AUTH_SOCK
 
+# Use 1Password as the provider of the ssh-agent
+# NB! Make sure to add the correct pam_gnome_kerying.so entries in /etc/pam.d/login
+pkill -f 'gnome-keyring-daemon'
+export SSH_AUTH_SOCK=~/.1password/agent.sock
+
 # See https://github.com/NixOS/nixpkgs/issues/14966#issuecomment-520083836
 mkdir -p "$HOME/.local/share/keyrings"
 
 # See https://unix.stackexchange.com/a/690294
-eval "$(echo '\n' | gnome-keyring-daemon --unlock)"
+# eval "$(echo '\n' | gnome-keyring-daemon --unlock)"
--- config/zsh/.zprofile
+++ config/zsh/.zprofile
@@ -3,7 +3,7 @@
 # Read more here: https://unix.stackexchange.com/a/487889
 
 # Start the gnome keyring manager deamon
-if command -v gnome-keyring-gnome-keyring-daemon 1>/dev/null 2>&1 && [ -n "$DESKTOP_SESSION" ];then
-    eval $(gnome-keyring-daemon --start)
-    export SSH_AUTH_SOCK
-fi
+# if command -v gnome-keyring-gnome-keyring-daemon 1>/dev/null 2>&1 && [ -n "$DESKTOP_SESSION" ];then
+#     eval $(gnome-keyring-daemon --start)
+#     export SSH_AUTH_SOCK
+# fi
