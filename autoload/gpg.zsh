b376c8310811af4bf51ba3073178cd51090f661c
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-08-25 12:08:14 +0200

    sway: Set correct DBUS environment

--- autoload/gpg.zsh
+++ autoload/gpg.zsh
@@ -1,14 +1,14 @@
 # Fix issue with VSCode not getting access to the system keychain
 # https://code.visualstudio.com/docs/editor/settings-sync#_troubleshooting-keychain-issues
-export $(dbus-launch)
+# export $(dbus-launch)
 
 # See https://wiki.archlinux.org/title/GNOME/Keyring#xinitrc
-# export $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
+# gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
 # export SSH_AUTH_SOCK
 
 # Use 1Password as the provider of the ssh-agent
 # NB! Make sure to add the correct pam_gnome_kerying.so entries in /etc/pam.d/login
-pkill -f 'gnome-keyring-daemon'
+# pkill -f 'gnome-keyring-daemon'
 export SSH_AUTH_SOCK=~/.1password/agent.sock
 
 # See https://github.com/NixOS/nixpkgs/issues/14966#issuecomment-520083836
