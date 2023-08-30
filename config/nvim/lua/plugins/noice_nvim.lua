28ecc02775da880ae5055bfee02792075ed9cd5d
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-08-30 17:27:55 +0200

    vim: Add noice.nvim plugin for prettier text input boxes

---
+++ config/nvim/lua/plugins/noice_nvim.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = true,
+      lsp_doc_border = true,
+    },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  }
+}
