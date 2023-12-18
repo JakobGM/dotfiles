14cf3cc146117e0e254cdaa3027f68a0d8b64f61
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-12-18 11:58:05 +0100

    vim: Add keybindings for Gp.nvim

--- config/nvim/lua/plugins/gp_nvim.lua
+++ config/nvim/lua/plugins/gp_nvim.lua
@@ -5,6 +5,17 @@ return {
       -- If missing, run the following command:
       -- op read op://Personal/fset4nvw2l6tvbufg6uovpty5e/api-key > ~/.config/chatgpt/secret.key
       openai_api_key = { "cat", "/home/jakobgm/.config/chatgpt/secret.key" },
+      chat_user_prefix = "💬:",
+      chat_conceal_model_params = false,
     })
   end,
+  keys = {
+    -- Invoke the GpChatToggle command
+    { "<C-g>t", ":GpChatToggle<CR>",    mode = { "n", "v" } },
+    { "<C-g>n", ":GpChatNew<CR>",       mode = { "n", "v" } },
+    { "<C-g>r", ":GpRewrite<CR>",       mode = { "v" } },
+    { "<C-g>a", ":GpAppend<CR>",        mode = { "v" } },
+    { "<C-g>d", "<cmd>GpChatDelete<CR>" },
+    { "<C-g>f", "<cmd>GpChatFinder<CR>" },
+  },
 }
