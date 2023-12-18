77470b7b81195ec20064f8c9f19c109613c9beff
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2023-12-18 11:01:36 +0100

    vim: Use file to store ChatGPT API key

--- config/nvim/lua/plugins/gp_nvim.lua
+++ config/nvim/lua/plugins/gp_nvim.lua
@@ -2,7 +2,9 @@ return {
   "robitx/gp.nvim",
   config = function()
     require("gp").setup({
-      openai_api_key = { "op", "read", "op://Personal/fset4nvw2l6tvbufg6uovpty5e/api-key", "--no-newline" },
+      -- If missing, run the following command:
+      -- op read op://Personal/fset4nvw2l6tvbufg6uovpty5e/api-key > ~/.config/chatgpt/secret.key
+      openai_api_key = { "cat", "/home/jakobgm/.config/chatgpt/secret.key" },
     })
   end,
 }
