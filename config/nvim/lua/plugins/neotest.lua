59672c20c4dc4b9965eca3d989db4ac348cc72fe
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2024-06-10 16:39:33 +0200

    vim: Fix neotest configuration for pytest

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -1,10 +1,11 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-neotest/neotest-python",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-neotest/neotest-python",
+    "nvim-treesitter/nvim-treesitter"
   },
   config = function()
     require("neotest").setup({
@@ -17,7 +18,7 @@ return {
     { "gtt", function() require("neotest").run.run() end },
     { "gtf", function() require("neotest").run.run(vim.fn.expand("%")) end },
     { "gtw", function() require("neotest").watch.toggle() end },
-    { "gtd", function() require("neotest").run.run({strategy = "dap"}) end },
+    { "gtd", function() require("neotest").run.run({ strategy = "dap" }) end },
     { "gta", function() require("neotest").run.attach() end },
   }
 }
