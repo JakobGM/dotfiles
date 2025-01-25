f6606d60056e15cb3632f803913b6b1a635fc380
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2025-01-25 20:31:47 +0100

    zsh: Remove unused environment variables

--- home/dot_config/zsh/env.zsh
+++ home/dot_config/zsh/env.zsh
@@ -1,8 +1,5 @@
 # The most important env variable of them all
-export PATH=$PATH:/Users/jakobgm/.cargo/bin:/Library/Frameworks/Python.framework/Versions/3.5/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:/opt/X11/bin:/Applications/Postgres.app/Contents/Versions/latest/bin
-
-# For use with X11 forwarding on remote python interpreterns
-# export DISPLAY=localhost:10.0
+export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 
 # pip should only run if there is a virtualenv currently activated
 export PIP_REQUIRE_VIRTUALENV=true  # But there are some global packages that are needed
@@ -13,12 +10,6 @@ export PROJECT_HOME="$HOME/dev"
 # For python [https://coderwall.com/p/-k_93g/mac-os-x-valueerror-unknown-locale-utf-8-in-python]
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-
-# The name of the python (mk)project which is currently the main project
-export MAIN_PYTHON_PROJECT='cloud'
-
-# Always use ptipython in shell_plus django-admin command
-export SHELL_PLUS='ptipython'
 
 # Set editor used by default by other programs, for instance tmuxinator
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
@@ -31,12 +22,6 @@ export EDITOR=$VISUAL
 
 # Add user binaries
 export PATH="$HOME/.local/share/chezmoi/bin:$PATH"
-
-# Add linuxbrew paths [http://linuxbrew.sh/]
-export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-
-# Add pip --user installed packages
-export PATH=`python3 -c 'import site; print(site.USER_BASE)'`/bin":$PATH"
 
 # Add cargo installed binaries
 export PATH="$HOME/.cargo/bin:$PATH"
@@ -54,17 +39,6 @@ export FZF_CTRL_T_OPTS="
 # Setting default username such that it doesn´t always state it in the terminal
 export DEFAULT_USER="jakobgm"
 
-# ENHANCD settings
-export ENHANCD_FILTER=fzf
-export ENHANCD_DIR="$XDG_CONFIG_HOME/enhancd"
-export ENHANCD_DISABLE_DOT=1
-export ENHANCD_DISABLE_DOT=1
-export ENHANCD_HOOK_AFTER_CD='ls -l'
-export ENHANCD_USE_FUZZY_MATCH=1
-
-# How to open MIME-types from terminal
-export MAILCAPS="$HOME/.mailcap"
-
 # Use the dotfiles-repo config folder as XDG Base Directory
 export XDG_CONFIG_HOME="$HOME/.config"
 
@@ -80,9 +54,6 @@ export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 # Use neovim as a manpager
 export MANPAGER="nvim -c 'set ft=neoman' -"
 
-# Set vimrc location
-export MYVIMRC="$XDG_CONFIG_HOME/nvim/init.vim"
-
 # Set default terminal
 export TERMINAL=kitty
 
@@ -90,18 +61,9 @@ export TERMINAL=kitty
 # "fatal: could not read Username for 'https://github.com': terminal prompts disabled"
 export GIT_TERMINAL_PROMPT=1
 
-# Log everything at debug level for astrality for development purposes
-export ASTRALITY_LOGGING_LEVEL=DEBUG
-
 # Allaw global packages install with npm -g without use of sudo
 export PATH="$HOME/.node_modules/bin:$PATH"
 export npm_config_prefix=~/.node_modules
-
-# Make the dotfiles repository the home for Astrality
-export ASTRALITY_CONFIG_HOME="$HOME/.local/share/chezmoi/"
-
-# Add ruby gems to path
-export PATH="$HOME/.gem/ruby/2.5.0/bin:${PATH}"
 
 
 # --- Fix lacking XDG Home Directory support ---
