74fd6bb51ad6a71ae5371edb76bb67a8180d9cfd
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2025-05-03 16:31:59 +0200

    vim: Remove unused Python ftplugin configuration

--- home/dot_config/nvim/ftplugin/python.lua
+++ home/dot_config/nvim/ftplugin/python.lua
@@ -6,20 +6,3 @@ vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
 -- Enable python highlighting
 vim.g.python_highlight_all = 1
-
--- " Format the current document with blackdoc
--- nnoremap <silent> <Leader>bd :w<CR>:!poetry run blackdoc %<CR>:e!<CR>
-vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>bd",
-  "<cmd>w<CR>:!poetry run blackdoc %<CR>:e!<CR>",
-  { noremap = true, silent = true }
-)
-
--- " Bind <Leader>f to run "ruff --fix"
-vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>f",
-  "<cmd>silent call CocAction('runCommand', 'ruff.executeAutofix')<CR>",
-  { noremap = true, silent = true }
-)
