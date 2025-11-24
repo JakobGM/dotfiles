deab4373e9f1c8b6bb446626582ccdb348052515
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2025-11-24 18:58:31 +0100

    vim: Remove render-markdown plugin

--- home/dot_config/nvim/lua/plugins/codecompanion_nvim.lua
+++ home/dot_config/nvim/lua/plugins/codecompanion_nvim.lua
@@ -3,7 +3,6 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter",
-    "render-markdown.nvim",
     "mini.diff",
   },
   opts = {
--- home/dot_config/nvim/lua/plugins/render_markdown_nvim.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  name = "render-markdown.nvim",
-  ft = { "markdown", "codecompanion" },
-}
